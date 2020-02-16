e5b480f8e6bfaa87580af7d60bbc91a75bca4dd2
YounessLahrour <44414654+YounessLahrour@users.noreply.github.com>
2020-02-16 20:39:31 +0100

    Single File Components

---
+++ TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+            <ul>
+                <todo-item
+                v-for="(todo,index) in todos"
+                :todo="todo">
+                </todo-item>
+            </ul>
+</template>
+
+<script>
+    export default {
+    name: 'todo-list',
+    props: ['todos'],
+    methods: {
+    
+    }
+</script>
+<style scoped>
+    ul {
+    list-style-type:none;
+    }
+</style>
